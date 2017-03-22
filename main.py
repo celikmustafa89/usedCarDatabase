febfd5b9fed8648dbc774ddda26e4dccb02fb141
Mustafa Çelik <Mustafa elik>
2017-03-22 17:30:30 +0300

    regression plotting codes are implemented

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
 
     # describe_data_frame(cars)  # creates table for the
 
-    data_cleaning_for_regression(cars)
+    clean_data = data_cleaning_for_regression(cars)
     plt.show()
     # Shows the first five records
     # print(">>> first five records:\n{}".format(cars.head()))
@@ -37,29 +37,47 @@ def main():
 def data_cleaning_for_regression(cars):
     """ Prepares the data for machine learning model.
 
-        dateCrawled:
+        # #################### COLUMNS THAT ARE COMPLETELY DELETED #################### #
+        dateCrawled: column is REMOVED.
+        nrOfPictures: has no value. column is REMOVED.
         name: all values are almost unique. So, it has no meaning for model. column is REMOVED.
         seller: has only 3 different values. So, it has no mean for model. rows and columns are REMOVED.
         offerType: has only 12 different values. rows and columns are REMOVED.
-        price: limits the price between 0< price < 100.000. rows are CLEANED.
         abtest: column REMOVED.
-        vehicleType:
-        yearOfRegistration: limits the year between 1900<year<2017. rows are CLEANED.
-        gearbox: FINE. missing rows are REMOVED.
-        powerPS: limits powerPS between 0<powerps<1000. rows are CLEANED.
-        model:
-        kilometer: FINE
         monthOfRegistration: has 13 values should be 12. not necessary for model. columns are REMOVED.
-        fuelType: FINE. missing rows are REMOVED.
-        brand:
-        notRepairedDamage:
         dateCreated: column is REMOVED.
-        nrOfPictures: has no value. column is REMOVED.
         postalCode: column is REMOVED.
-        lastSeen:
+        lastSeen: column is REMOVED.
+
+        # #################### COLUMNS THAT ARE PARTIALLY DELETED #################### #
+        price: limits the price between 0< price < 100.000. rows are CLEANED.
+        yearOfRegistration: limits the year between 1900<year<2017. rows are CLEANED.
+        powerPS: limits powerPS between 0<powerps<1000. rows are CLEANED.
+        fuelType: 5 type of values are removed other than "benzin" and "diesel". missing rows are REMOVED.
+
+        # #################### COLUMNS THAT HAVE CONSISTENT VALUES #################### #
+        vehicleType: FINE
+        model: FINE
+        kilometer: FINE
+        gearbox: FINE. missing rows are REMOVED.
+        brand: FINE
+        notRepairedDamage: FINE
+
     """
 
-    clean_data = cars.drop('nrOfPictures', 1)  # pictures column has no value. Removes the picture column
+    # #################### COLUMNS THAT ARE COMPLETELY DELETED #################### #
+
+    # dateCrawled does not effect model. Removes the dateCrawled column
+    clean_data = cars.drop('dateCrawled', 1)
+
+    # pictures column has no value. Removes the picture column
+    clean_data = clean_data.drop('nrOfPictures', 1)
+
+    # name column has 233531 different value over 371528.
+    # So, name column is not a good value for our model.
+    # There can be extracted some useful information by NLP.
+    # However, I am removing this column for my model.
+    clean_data = clean_data.drop('name', 1)  # removes the name column
 
     # prints the seller column's information
     # ### print(clean_data.groupby('seller').size())
@@ -76,57 +94,14 @@ def data_cleaning_for_regression(cars):
     clean_data = clean_data[clean_data.offerType != 'Gesuch']  # removes the gesuch rows
     clean_data = clean_data.drop('offerType', 1)  # removes the offerType column
 
-    # name column has 233531 different value over 371528.
-    # So, name column is not a good value for our model.
-    # There can be extracted some useful information by NLP.
-    # However, I am removing this column for my model.
-    clean_data = clean_data.drop('name', 1)  # removes the name column
-
     # based on the comment on the website, it should be better to remove this column
     clean_data = clean_data.drop('abtest', 1)  # removes the abtest column
-
-    # 10000 sample data are chosen for visualization
-    # sample_data = clean_data.sample(n=10000, random_state=1)
-
-    # price histogram graph
-    # plt.subplot(3, 1, 1)
-    # sample_data['price'].hist(bins=50)
-    # plt.title('Histogram price')
-
-    # for removing the overestimated price. limits the max price 100.000
-    clean_data = clean_data[clean_data.price < 100000]
-    sample_data = clean_data.sample(n=10000, random_state=1)
-
-    # 10772 cars are free. So, it is not a consistent value for the model. price=0 rows are removed.
-    # ### print(len(clean_data[clean_data.price == 0]), 'cars with price 0')
-    clean_data = clean_data[clean_data.price != 0]
-    sample_data = clean_data.sample(n=10000, random_state=1)
-
-    # plt.subplot(3, 1, 3)
-    # sample_data['price'].hist(bins=50)
-    # plt.title('Final-Histogram price(0 and >100.000 price are removed)')
-    # plt.show()
-
-    # yearOfRegistration column has some inconsistent values.
-    # To reduces the number of the inconsistent rows, limit  year between 1900<year<2017
-    clean_data = clean_data[(clean_data.yearOfRegistration >= 1900) & (clean_data.yearOfRegistration < 2017)]
-
-    # powerPS column has inconsistent values.
-    # To reduces the number of the inconsistent rows, limit powerPS between 0<powerPS<1000
-    clean_data = clean_data[(clean_data.powerPS > 0) & (clean_data.powerPS < 1000)]
-
-    # Kilometer data looks fine
-    # ### draw_column_histogram(clean_data, 'kilometer')
 
     # month has 13 unique values. It should have 12 values.
     # month values is not necessary for the price prediction.
     # So, it can be removed.
     # ### draw_column_histogram(clean_data, 'monthOfRegistration')
     clean_data = clean_data.drop('monthOfRegistration', 1)  # removes the month column
-
-
-    # ### print(clean_data.head)
-    # ### print("cars: {}\nclean_Data: {}".format(cars.shape, clean_data.shape))
 
     # to have better understanding postalCode should be matched with location in Germany.
     # Location of the cars in Germany will not effect the price of the car.
@@ -138,8 +113,73 @@ def data_cleaning_for_regression(cars):
     # print(clean_data['dateCreated'].describe())
     clean_data = clean_data.drop('dateCreated', 1)  # removes the dateCreated column
 
-    draw_column_histogram_string(clean_data, 'brand')
+    # lastSeen column as 157877 unique value. Has no effect on model.
+    # So, lastSeen column is removed
+    # ### draw_column_histogram_string(clean_data, 'lastSeen') # too many data for histogram
+    clean_data = clean_data.drop('lastSeen', 1)  # removes the lastSeen column
 
+    # #################### COLUMNS THAT ARE PARTIALLY DELETED #################### #
+
+    # for removing the overestimated price. limits the max price 100.000
+    clean_data = clean_data[clean_data.price < 100000]
+    # 10772 cars are free. So, it is not a consistent value for the model. price=0 rows are removed.
+    # ### print(len(clean_data[clean_data.price == 0]), 'cars with price 0')
+    clean_data = clean_data[clean_data.price != 0]
+
+    # yearOfRegistration column has some inconsistent values.
+    # To reduces the number of the inconsistent rows, limit  year between 1900<year<2017
+    clean_data = clean_data[(clean_data.yearOfRegistration >= 1900) & (clean_data.yearOfRegistration < 2017)]
+
+    # powerPS column has inconsistent values.
+    # To reduces the number of the inconsistent rows, limit powerPS between 0<powerPS<1000
+    clean_data = clean_data[(clean_data.powerPS > 0) & (clean_data.powerPS < 1000)]
+
+    # fuelType has 7 unique values. benzin and diesel dominates the other values.
+    # to have better values and simplify the data set, other values are removed.
+    # ### draw_column_histogram_string(clean_data, 'fuelType')
+    clean_data = clean_data[clean_data.fuelType != 'lpg']  # removes the  rows
+    clean_data = clean_data[clean_data.fuelType != 'cng']  # removes the  rows
+    clean_data = clean_data[clean_data.fuelType != 'hybrid']  # removes the  rows
+    clean_data = clean_data[clean_data.fuelType != 'andere']  # removes the  rows
+    clean_data = clean_data[clean_data.fuelType != 'elektro']  # removes the  rows
+    # ### draw_column_histogram_string(clean_data, 'fuelType')
+
+    # #################### COLUMNS THAT HAVE CONSISTENT VALUES #################### #
+
+    # vehicleType looks fine.
+    # ### draw_column_histogram_string(clean_data, 'vehicleType')
+
+    # model looks fine.
+    # ### draw_column_histogram_string(clean_data, 'model')
+
+    # kilometer data looks fine
+    # ### draw_column_histogram(clean_data, 'kilometer')
+
+    # gearbox name histogram shows that the values are ok.
+    # ### draw_column_histogram_string(clean_data, 'gearbox')
+
+    # notRepairedDamage name histogram shows that the values are ok.
+    # ### draw_column_histogram_string(clean_data, 'notRepairedDamage')
+
+    # brand name histogram shows that the values are ok.
+    # ### draw_column_histogram_string(clean_data, 'brand')
+
+    clean_data = clean_data.dropna()  # removes the rows that has missing values
+
+    #print(clean_data.head)
+    print("cars: {}\nclean_Data: {}".format(cars.shape, clean_data.shape))
+
+    """
+    # Create a Pandas Excel writer using XlsxWriter as the engine.
+    writer = pd.ExcelWriter('output/clean_data.xlsx', engine='xlsxwriter')
+    # Convert the dataframe to an XlsxWriter Excel object.
+    clean_data.to_excel(writer, sheet_name='clean_data')
+
+    # Close the Pandas Excel writer and output the Excel file.
+    writer.save()
+    """
+
+    return clean_data
 
 def regression_model():
     pass
@@ -209,7 +249,9 @@ def draw_column_histogram_string(data_frame, column_name):
     print(data_frame[column_name].describe())
 
     # sample_data = data_frame.sample(n=10000, random_state=1)
-    data_frame.apply(pd.value_counts)
+    values = pd.Series(data_frame[column_name])
+    d = pd.DataFrame({column_name:values})
+    d.apply(pd.value_counts).plot(kind='bar', subplots=True)
     #plt.title('Histogram ' + column_name)
     plt.show()
 
